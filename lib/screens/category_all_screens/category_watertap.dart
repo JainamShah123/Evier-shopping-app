ee4fde98e28bc2d0de9232752e8325e549830fb8
JainamShah123 <46788239+JainamShah123@users.noreply.github.com>
2021-03-26 16:53:35 +0530

    minor change

--- lib/screens/category_all_screens/category_watertap.dart
+++ lib/screens/category_all_screens/category_watertap.dart
@@ -29,45 +29,48 @@ class WaterTap extends StatelessWidget {
                     margin: EdgeInsets.all(25),
                     child: Image.asset("assests/8.jpg"),
                   ),
-                  Row(mainAxisAlignment: MainAxisAlignment.end, children: [
-                    Column(
-                      children: [
-                        Container(
-                          margin: EdgeInsets.only(right: 15, top: 10),
-                          child: Text(
-                            "Company Name: Jaquar",
-                            style: TextStyle(fontSize: 20),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Column(
+                        children: [
+                          Container(
+                            margin: EdgeInsets.only(right: 15, top: 10),
+                            child: Text(
+                              "Company Name: Jaquar",
+                              style: TextStyle(fontSize: 20),
+                            ),
                           ),
-                        ),
-                        Container(
-                          margin: EdgeInsets.only(right: 23, top: 5),
-                          child: Text(
-                            "Product Name: WaterTap",
-                            style: TextStyle(fontSize: 20),
+                          Container(
+                            margin: EdgeInsets.only(right: 23, top: 5),
+                            child: Text(
+                              "Product Name: WaterTap",
+                              style: TextStyle(fontSize: 20),
+                            ),
                           ),
-                        ),
-                        Container(
-                          margin: EdgeInsets.only(right: 140, top: 5),
-                          child: Text(
-                            "MRP: 499",
-                            style: TextStyle(fontSize: 20, color: Colors.red),
+                          Container(
+                            margin: EdgeInsets.only(right: 140, top: 5),
+                            child: Text(
+                              "MRP: 499",
+                              style: TextStyle(fontSize: 20, color: Colors.red),
+                            ),
                           ),
-                        ),
-                        ElevatedButton(
-                          onPressed: () {
-                            Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                    builder: (ctx) => CartScreen()));
-                          },
-                          child: Text(
-                            "Go to Cart Page",
-                            style: TextStyle(color: Colors.white),
+                          ElevatedButton(
+                            onPressed: () {
+                              Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (ctx) => CartScreen()));
+                            },
+                            child: Text(
+                              "Go to Cart Page",
+                              style: TextStyle(color: Colors.white),
+                            ),
                           ),
-                        ),
-                      ],
-                    ),
-                  ])
+                        ],
+                      ),
+                    ],
+                  )
                 ],
               ),
             ),
